8212f85802a07e8b7531b8013532e3ac4edea93d
Maya Zakharova <mayzakharova@gmail.com>
2025-12-24 15:11:08 -0600

    add routes for boards

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,70 @@ import BoardList from './components/boards/BoardList';
 import NewBoardForm from './components/boards/NewBoardForm';
 import CardList from './components/cards/CardList';
 import NewCardForm from './components/cards/NewCardForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css'
 
 // Board   -  { id, title, name, cards }
-// Card   -  { id, message, likes }
+// Card   -  { id, message, likes, board }
 
-const BOARDS = [
-  { id: 1, title: 'Travel', name: 'Maya', cards: [] },
-  { id: 2, title: 'Ideas', name: 'Mumina', cards: [] },
-  { id: 3, title: 'Cooking', name: 'Wenxin', cards: [] },
-  { id: 4, title: 'Sport', name: 'Hsiang-ting', cards: [] },
-];
+// const BOARDS = [
+//   { id: 1, title: 'Travel', name: 'Maya', cards: [] },
+//   { id: 2, title: 'Ideas', name: 'Mumina', cards: [] },
+//   { id: 3, title: 'Cooking', name: 'Wenxin', cards: [] },
+//   { id: 4, title: 'Sport', name: 'Hsiang-ting', cards: [] },
+// ];
+
+const kbaseURL = 'http://localhost:5000';
+
+const getAllBoardsAPI = () => {
+  return axios.get(`${kbaseURL}/boards`)
+    .then(response => response.data.boards)
+    .catch(error => console.log(error));
+};
+
+const getBoardByIdAPI = (id) => {
+  return axios.get(`${kbaseURL}/boards/${id}`)
+    .then(response => response.data)
+    .catch(error => console.log(error));
+};
+
+const addBoardByAPI = (newBoard) => {
+  return axios.post(`${kbaseURL}/boards`, newBoard)
+    .catch(error => console.log(error));
+};
+
+const deleteBoardAPI = id => {
+  return axios.delete(`${kbaseURL}/boards/${id}`)
+    .catch(error => console.log(error));
+};
+
 
 function App() {
-  const [boards, setBoards] = useState(BOARDS);
+  const [boards, setBoards] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState(null);
 
+  useEffect(() => {
+    getAllBoardsAPI().then(data => setBoards(data));
+  }, []);
+
+  const handleSelectBoard = (board) => {
+    getBoardByIdAPI(board.id).then(data => setSelectedBoard(data));
+  }
+
   const addBoard = (newBoard) => {
-    setBoards(prev => [...prev, newBoard])
+    addBoardByAPI(newBoard).then(response => {
+      if(response && response.data) {
+        setBoards(prev => [...prev, newBoard]);
+      }
+    })   
   }
 
   const deleteBoard = (boardId) => {
-    setBoards(prev => prev.filter(board => board.id !== boardId));
-
-    if(selectedBoard?.id === boardId) {
-      setSelectedBoard(null);
-    }
+    deleteBoardAPI(boardId).then(() => {
+      setBoards(prev => prev.filter(board => board.id !== boardId));
+      if(selectedBoard?.id === boardId) setSelectedBoard(null);
+    });
   }
 
   return (
@@ -41,7 +78,7 @@ function App() {
           <BoardList 
           boards={boards}
           selectedBoard={selectedBoard}
-          onSelectBoard={setSelectedBoard}
+          onSelectBoard={handleSelectBoard}
           onDeleteBoard={deleteBoard}
           />
         </section>

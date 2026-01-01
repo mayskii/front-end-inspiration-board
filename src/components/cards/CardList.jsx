a157543fe129b493bd53fe3948e5fe235e8ffdcc
Mumina <muminaabdi13@gmail.com>
2025-12-31 22:22:14 -0500

    implementation to card component,cards working

--- src/components/cards/CardList.jsx
+++ src/components/cards/CardList.jsx
@@ -1,9 +1,37 @@
-const CardList = () =>{
-    return (
-        <>
-        <h2>CardList</h2>
-        </>
-    )
+
+import Card from "./Card";
+import PropTypes from "prop-types";
+import './Cards.css';
+
+const CardList = ({ cards, boardTitle, onDeleteCard, onLikeCard }) =>{
+  return (
+    <> 
+      <h3>{boardTitle}</h3>
+      <ul className='card-list'>
+        {cards.map(card => (
+          <Card 
+            key={card.id}
+            card={card}
+            onLikeCard={onLikeCard}
+            onDelete={onDeleteCard}
+          />
+        ))}
+      </ul>
+    </>
+  )
 }
+
+CardList.propTypes = {
+    cards: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            message: PropTypes.string.isRequired,
+            likes: PropTypes.number,
+        })
+    ).isRequired,
+    boardTitle: PropTypes.string.isRequired,
+    onDeleteCard: PropTypes.func.isRequired,
+    onLikeCard: PropTypes.func.isRequired,
+};
 
 export default CardList;
